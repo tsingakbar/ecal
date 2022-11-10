84e686923db7ac2213e5a294afe2310e2dc1cc10
richieyu <richieyu@tencent.com>
2022-11-10 12:15:04 +0800

    wireshark ecal dissector add udp message support

--- app/mon/wireshark_dissector/ecal.lua
+++ app/mon/wireshark_dissector/ecal.lua
@@ -4,51 +4,91 @@
 --    place this lua plugin to this folder so wireshark can load it.
 
 
-ecal_udp_sample_protocol = Proto("EcalUdpSample",  "Ecal UDP Sample Protocol")
+ecal_udp_discovery_protocol = Proto("Ecal_Udp_Discovery",  "Ecal UDP Discovery Protocol")
 
-field_header_mark = ProtoField.string("header.ECAL")
-field_header_version = ProtoField.uint32("header.version")
-field_header_type = ProtoField.uint32("header.type")
-field_header_id = ProtoField.int32("header.id")
-field_header_num = ProtoField.uint32("header.num")
-field_header_length = ProtoField.uint32("header.body_length")
-field_sample_name_length = ProtoField.uint16("sample.name_length")
-field_sample_name = ProtoField.string("sample.name")
+discovery_field_header_mark = ProtoField.string("header.ECAL")
+discovery_field_header_version = ProtoField.uint32("header.version")
+discovery_field_header_type = ProtoField.uint32("header.type")
+discovery_field_header_id = ProtoField.int32("header.id")
+discovery_field_header_num = ProtoField.uint32("header.num")
+discovery_field_header_length = ProtoField.uint32("header.body_length")
+discovery_field_sample_name_length = ProtoField.uint16("sample.name_length")
+discovery_field_sample_name = ProtoField.string("sample.name")
 
-ecal_udp_sample_protocol.fields = {
-  field_header_mark,
-  field_header_version,
-  field_header_type,
-  field_header_id,
-  field_header_num,
-  field_header_length,
-  field_sample_name_length,
-  field_sample_name,
+ecal_udp_discovery_protocol.fields = {
+  discovery_field_header_mark,
+  discovery_field_header_version,
+  discovery_field_header_type,
+  discovery_field_header_id,
+  discovery_field_header_num,
+  discovery_field_header_length,
+  discovery_field_sample_name_length,
+  discovery_field_sample_name,
 }
 
 local protobuf_dissector = Dissector.get("protobuf")
-local identify_port = 14000
+local discovery_port = 14000
 
-function ecal_udp_sample_protocol.dissector(buffer, pinfo, tree)
+function ecal_udp_discovery_protocol.dissector(buffer, pinfo, tree)
   -- only match destination port
-  if pinfo.dst_port ~= identify_port then return end
+  if pinfo.dst_port ~= discovery_port then return end
   if buffer:len() == 0 then return end
 
-  pinfo.cols.protocol = ecal_udp_sample_protocol.name
+  pinfo.cols.protocol = ecal_udp_discovery_protocol.name
 
-  local subtree = tree:add(ecal_udp_sample_protocol, buffer(), "Ecal UDP Sample Data")
-  subtree:add_le(field_header_mark, buffer(0, 4))
-  subtree:add_le(field_header_version, buffer(4, 4))
-  subtree:add_le(field_header_type, buffer(8, 4))
-  subtree:add_le(field_header_id, buffer(12, 4))
-  subtree:add_le(field_header_num, buffer(16, 4))
-  subtree:add_le(field_header_length, buffer(20, 4))
-  subtree:add_le(field_sample_name_length, buffer(24, 2))
+  local subtree = tree:add(ecal_udp_discovery_protocol, buffer(), "Ecal UDP Discovery Data")
+  subtree:add_le(discovery_field_header_mark, buffer(0, 4))
+  subtree:add_le(discovery_field_header_version, buffer(4, 4))
+  subtree:add_le(discovery_field_header_type, buffer(8, 4))
+  subtree:add_le(discovery_field_header_id, buffer(12, 4))
+  subtree:add_le(discovery_field_header_num, buffer(16, 4))
+  subtree:add_le(discovery_field_header_length, buffer(20, 4))
+  subtree:add_le(discovery_field_sample_name_length, buffer(24, 2))
   local sample_name_length = buffer(24, 2):le_uint()
-  subtree:add_le(field_sample_name, buffer(26, sample_name_length))
+  subtree:add_le(discovery_field_sample_name, buffer(26, sample_name_length))
 
   pinfo.private["pb_msg_type"] = "message,eCAL.pb.Sample"
   pcall(Dissector.call, protobuf_dissector, buffer(26+sample_name_length):tvb(), pinfo, tree)
 end
 
-DissectorTable.get("udp.port"):add(identify_port, ecal_udp_sample_protocol)
+DissectorTable.get("udp.port"):add(discovery_port, ecal_udp_discovery_protocol)
+
+------------------------------------------------------------------------------------
+
+ecal_udp_message_protocol = Proto("Ecal_Udp_Message",  "Ecal UDP Message Protocol")
+
+message_field_header_mark = ProtoField.string("header.ECAL")
+message_field_header_version = ProtoField.uint32("header.version")
+message_field_header_type = ProtoField.uint32("header.type")
+message_field_header_id = ProtoField.int32("header.id")
+message_field_header_num = ProtoField.uint32("header.num")
+message_field_header_length = ProtoField.uint32("header.body_length")
+
+ecal_udp_message_protocol.fields = {
+  message_field_header_mark,
+  message_field_header_version,
+  message_field_header_type,
+  message_field_header_id,
+  message_field_header_num,
+  message_field_header_length,
+}
+
+local message_port = 14002
+function ecal_udp_message_protocol.dissector(buffer, pinfo, tree)
+  -- only match destination port
+  if pinfo.dst_port ~= message_port then return end
+  if buffer:len() == 0 then return end
+
+  pinfo.cols.protocol = ecal_udp_message_protocol.name
+
+  local subtree = tree:add(ecal_udp_message_protocol, buffer(0, 24), "Ecal UDP Message Header")
+  subtree:add_le(message_field_header_mark, buffer(0, 4))
+  subtree:add_le(message_field_header_version, buffer(4, 4))
+  subtree:add_le(message_field_header_type, buffer(8, 4))
+  subtree:add_le(message_field_header_id, buffer(12, 4))
+  subtree:add_le(message_field_header_num, buffer(16, 4))
+  subtree:add_le(message_field_header_length, buffer(20, 4))
+  local subtree_body = tree:add(ecal_udp_message_protocol, buffer(24), "Ecal UDP Message Body")
+end
+
+DissectorTable.get("udp.port"):add(message_port, ecal_udp_message_protocol)
