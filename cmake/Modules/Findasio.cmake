013c2956a63a3f772feac769498f339389d76253
richieyu <richieyu@tencent.com>
2023-03-02 22:39:34 +0800

    prefer system provided asio unless it's too old

--- cmake/Modules/Findasio.cmake
+++ cmake/Modules/Findasio.cmake
@@ -1,31 +1,38 @@
-find_path(asio_INCLUDE_DIR
-  NAMES asio.hpp
-  HINTS
-    "${CONAN_ASIO_ROOT}/include"
-    "${ECAL_PROJECT_ROOT}/thirdparty/asio/asio/include"
-  NO_DEFAULT_PATH
-  NO_CMAKE_FIND_ROOT_PATH
-)
-
-if(asio_INCLUDE_DIR-NOTFOUND)
-  message(FATAL_ERROR "Could not find asio library")
-  set(asio_FOUND FALSE)
+# prefer system provided asio unless it's too old
+find_path(asio_INCLUDE_DIR asio.hpp)
+if (asio_INCLUDE_DIR-NOTFOUND)
+    set(asio_INCLUDE_DIR "${ECAL_PROJECT_ROOT}/thirdparty/asio/asio/include")
 else()
-  set(asio_FOUND TRUE)
-  set(ASIO_INCLUDE_DIR ${asio_INCLUDE_DIR})
+    # Matches a line of the form:
+    #
+    # #define ASIO_VERSION XXYYZZ // XX.YY.ZZ
+    #
+    # with arbitrary whitespace between the tokens
+    file(
+      STRINGS "${asio_INCLUDE_DIR}/asio/version.hpp" ASIO_VERSION_DEFINE_LINE
+      REGEX
+        "#define[ \t]+ASIO_VERSION[ \t]+[0-9]+[ \t]+//[ \t]+[0-9]+\.[0-9]+\.[0-9]+[ \t]*"
+    )
+    # Extracts the dotted version number after the comment as ASIO_VERSION_STRING
+    string(REGEX
+           REPLACE "#define ASIO_VERSION [0-9]+ // ([0-9]+\.[0-9]+\.[0-9]+)"
+                   "\\1" ASIO_VERSION_STRING "${ASIO_VERSION_DEFINE_LINE}"
+    )
+    if(ASIO_VERSION_STRING VERSION_LESS "1.12.2")
+      # system provided asio too old, use the bundled version
+      set(asio_INCLUDE_DIR "${ECAL_PROJECT_ROOT}/thirdparty/asio/asio/include")
+    endif()
 endif()
 
-if(asio_FOUND)
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(asio
-    REQUIRED_VARS asio_INCLUDE_DIR)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(asio
+  REQUIRED_VARS asio_INCLUDE_DIR)
 
-  if(NOT TARGET asio::asio)
-    set(asio_INCLUDE_DIRS ${asio_INCLUDE_DIR})
-    add_library(asio::asio INTERFACE IMPORTED)
-    set_target_properties(asio::asio PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES ${asio_INCLUDE_DIR}
-      INTERFACE_COMPILE_DEFINITIONS ASIO_STANDALONE)
-    mark_as_advanced(asio_INCLUDE_DIR)
-  endif()
+if(NOT TARGET asio::asio)
+  set(asio_INCLUDE_DIRS ${asio_INCLUDE_DIR})
+  add_library(asio::asio INTERFACE IMPORTED)
+  set_target_properties(asio::asio PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES ${asio_INCLUDE_DIR}
+    INTERFACE_COMPILE_DEFINITIONS ASIO_STANDALONE)
+  mark_as_advanced(asio_INCLUDE_DIR)
 endif()
